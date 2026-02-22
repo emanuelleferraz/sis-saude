fbe8c007865138dc1588c347c50b50983af9efb9
Emanuelle Ferraz <manu.ferraz16@gmail.com>
2026-02-22 12:09:31 -0300

    🎉commit inicial

---
+++ sis-api/src/test/java/br/edu/ufop/tcc/sis_api/SisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufop.tcc.sis_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
